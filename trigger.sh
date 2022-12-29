7bfc08f896bfd69ace23caa8c62f30b3073f3464
indraay022 <indraay022@gmail.com>
2022-12-29 23:53:33 +0700

    New version

--- trigger.sh
+++ trigger.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 sh txt-builder.sh
-status=$(<status.txt)
+status=$(<storage/status.txt)
 curl -X POST 'https://api.telegram.org/bot5938873593:AAHot--rlj_P7O0Zu6jfbjvkwbgocFwXg_M/sendMessage' \
  -d chat_id=689840928 \
- -d text="$status" \
- -d parse_mode=MarkdownV2
+ -d text="$status" 
